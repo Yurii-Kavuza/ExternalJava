c0e9fca725f6a8bd8ff5735828d9a4ae78807e81
Yurii-Kavuza <kavuzaiurii@.gmail.com>
2020-02-11 20:11:11 +0200

    DoublePowerDroid extends AthleteDroid

--- Droids/src/ua/external/base/oop/droid/droids/DoublePowerDroid.java
+++ Droids/src/ua/external/base/oop/droid/droids/DoublePowerDroid.java
@@ -1,7 +1,8 @@
 package ua.external.base.oop.droid.droids;
 
-public class DoublePowerDroid extends Droid{
+public class DoublePowerDroid extends AthleteDroid{
     public DoublePowerDroid(int health, int energy, int damage, String name) {
         super(health, energy, damage, name);
+        super.setMultiplyPower(2);
     }
 }
