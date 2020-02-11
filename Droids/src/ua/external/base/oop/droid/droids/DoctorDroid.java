1942afe6e5f07e809436ed9c7c43bc06c7ff77a8
Kateryna Koop <koopkateryna@gmail.com>
2020-02-11 15:02:25 +0200

    Added DoctorDroid.java

---
+++ Droids/src/ua/external/base/oop/droid/droids/DoctorDroid.java
@@ -0,0 +1,44 @@
+public class DoctorDroid extends Droid 
+{
+	private int criticalLevelOfHealth = 10;
+
+
+	public DoctorDroid(int health, int energy, int damage, String name)
+	{
+		super(health, energy, damage, name);
+	}
+
+	public DoctorDroid(Droid patient)
+	{
+		medicalExamination(patient);
+	}
+
+
+	public void medicalExamination(Droid patient)
+	{
+		if(patient.getHealth() <= criticalLevelOfHealth)
+		{
+			treatPatient(patient);
+		}
+		else
+		{
+			System.out.println("You are healthy! :)");
+		}
+	}
+
+	public void treatPatient(Droid patient)
+	{
+		setHealth(patient.getHealth() * 2);
+	}
+
+
+	public void getCriticalLevelOfHealth()
+	{
+		return criticalLevelOfHealth;
+	}
+
+	public int setCriticalLevelOfHealth(int criticalLevelOfHealth)
+	{
+		this.criticalLevelOfHealth = criticalLevelOfHealth;
+	}
+}
