93c496475127664439b23842a90bbf50a5036e51
Yurii-Kavuza <kavuzaiurii@.gmail.com>
2020-02-11 16:52:24 +0200

    Fixing SelfHealthingDroid

--- Droids/src/ua/external/base/oop/droid/droids/SelfHealingDroid.java
+++ Droids/src/ua/external/base/oop/droid/droids/SelfHealingDroid.java
@@ -3,8 +3,8 @@ package ua.external.base.oop.droid.droids;
 public class SelfHealingDroid extends Droid {
     private double recoveryRatio;
 
-    public SelfHealingDroid(int energy, int damage, int health, double recoveryRatio) {
-        super(energy, damage, (int) (health * recoveryRatio));
+    public SelfHealingDroid(int health, int energy, int damage, String name, double recoveryRatio) {
+        super((int) (health * recoveryRatio),energy, damage,name);
         this.recoveryRatio = recoveryRatio;
     }
 
